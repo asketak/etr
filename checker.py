25a1834520fdc726983470e885638b291ad6bede
asketak <simatomas@seznam.cz>
2018-10-27 22:00:12 +0200

    req

--- checker.py
+++ checker.py
@@ -20,10 +20,10 @@ from contextlib import contextmanager
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support.expected_conditions import staleness_of
 
-def send_notification(img):
+def send_notification(title,img):
     url = 'https://www.pushsafer.com/api'
     post_fields = { 
-        "t" : "ETORO ZMENA",
+        "t" : title,
         "m" : "https://www.etoro.com/people/aimstrader/portfolio",
         "s" : 10,
         "v" : 3,
@@ -46,13 +46,17 @@ def screenshot():
     # cmd = "firefox -screenshot https://www.etoro.com/people/aimstrader/portfolio "
     # os.system(cmd)
 
-        adresa = ' https://www.etoro.com/people/aimstrader/portfolio '
+        # adresa = ' https://www.etoro.com/people/aimstrader/portfolio '
+        adresa = 'https://www.etoro.com/people/asketak/portfolio '
         options = webdriver.ChromeOptions()
 
-        options.add_argument('headless')
+        # options.add_argument('headless')
         options.add_argument('window-size=1200x600')
+        options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36")
 
-        driver = webdriver.Chrome('./chromedriver')
+        driver = webdriver.Chrome('./chromedriver', chrome_options=options)
+        profile = webdriver.FirefoxProfile()
+        driver = webdriver.Firefox(profile)
         driver.get(adresa)
 
         driver.implicitly_wait(30)
@@ -79,20 +83,27 @@ def are_images_same(img1,img2):
     return False
 
 if __name__ == "__main__":
-    try:
-        display = Display(visible=0, size=(1800, 1600))
-        display.start()
-        while True:
+    # display = Display(visible=0, size=(1800, 1600))
+    # display.start()
+    sendimg =  base64.b64encode(open("screenshot.png", "rb").read()).decode('UTF-8')
+    sendimg = "data:image/png;base64," + sendimg
+    while True:
+        try:
             print("loop")
-            screenshot()
+            try:
+                screenshot()
+            except Exception as e:
+                print(e)
+                send_notification("ETORO-screenshot",sendimg) # TODO musi byt alert i pri navraceni,
+                continue
             if not are_images_same("before_screenshot.png", "screenshot.png"):
                 os.system("cp screenshot.png before_screenshot.png")
                 sendimg =  base64.b64encode(open("screenshot.png", "rb").read()).decode('UTF-8')
                 sendimg = "data:image/png;base64," + sendimg
-                send_notification(sendimg) # TODO musi byt alert i pri navraceni,
+                send_notification("ETORO-ZMENA",sendimg) # TODO musi byt alert i pri navraceni,
 
             time.sleep(60)
-    except Exception as e:
-        send_notification("") # TODO musi byt alert i pri navraceni,
-        print(e)
+        except Exception as e:
+            send_notification("ETORO - crash",sendimg) # TODO musi byt alert i pri navraceni,
+            print(e)
 
